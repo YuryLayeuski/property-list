9e2e5266a08d22229571b075d54fe1a11a9e433b
Yury Layeuski <yury@residenetwork.com>
2021-04-15 23:25:42 -0700

    add some comments

--- src/components/Property.js
+++ src/components/Property.js
@@ -2,11 +2,13 @@ import React from 'react'
 import {ReactComponent as HeartFill} from '../assets/heart-fill.svg';
 import {ReactComponent as HeartStroke} from '../assets/heart-stroke.svg';
 
-
 const Property = ({photos, listPrice, property, address, listDate, handleFavorite, mlsId}) => {
+  // convert listed date
   const listedDate = new Date(listDate).toLocaleDateString("en-US");
+  // calculate total baths with a partial baths
   const totalBath = property.bathsFull + property.bathsHalf * 0.5;
 
+  // get the favorite properties from local storage
   let favoriteProperty;
   const favoriteDataFromLocalStorage = localStorage.getItem('favorite');
   if(favoriteDataFromLocalStorage){
@@ -21,7 +23,11 @@ const Property = ({photos, listPrice, property, address, listDate, handleFavorit
         <div className='container'>
           <img className='image' src={photos[0]} alt='property'/>
           <button onClick={handleFavorite} className='icon-heart'>
-            {favoriteProperty.indexOf(mlsId) !== -1 ? <HeartFill data-testid="favorite"/> : <HeartStroke data-testid="not-favorite"/>}
+            {
+              // display red color heart based on the favorite properties from local storage
+              // added 'data-testid' attribute for the cypress tests
+              favoriteProperty.indexOf(mlsId) !== -1 ? <HeartFill data-testid="favorite"/> : <HeartStroke data-testid="not-favorite"/>
+            }
           </button>
         </div>
         <div className='property-footer'>
--- src/components/PropertyList.js
+++ src/components/PropertyList.js
@@ -3,6 +3,7 @@ import Loading from './Loading';
 import Property from './Property';
 
 const url = 'https://api.simplyrets.com/properties';
+// storing username and password inside .env file
 const username = process.env.REACT_APP_USERNAME;
 const password = process.env.REACT_APP_PASSWORD;
 // get properties data from localStorage if data exists
@@ -14,7 +15,7 @@ const getStorageProperties = () => {
     return [];
   }
 };
-
+// get favorite property data from localStorage if data exists
 const getStorageFavorite = () => {
   let favorite = localStorage.getItem('favorite');
   if (favorite) {
@@ -27,12 +28,13 @@ const getStorageFavorite = () => {
 
 function PropertyList() {
   const [loading, setLoading] = useState(false);
-  // get the initial state from the localStorage cache if it exists
-  // otherwise fetch data
+  // get initial state from localStorage cache if it exists, otherwise fetch data
   const [properties, setProperties] = useState(getStorageProperties());
+  // get favorite data from localStorage cache if it exists, otherwise initial state will be empty array
   const [favorite, setFavorite] = useState(getStorageFavorite());
 
   const fetchProperties = async () => {
+    // display loading while fetching data
     setLoading(true)
     try {
       const response = await fetch(url, {
@@ -43,8 +45,11 @@ function PropertyList() {
         },
       });
       const data = await response.json();
-      // Storage.setObj('properties', data)
-      localStorage.setItem('properties', JSON.stringify(data));
+      // set data from simplyRets to local storage
+      if(data){
+        localStorage.setItem('properties', JSON.stringify(data));
+      }
+      // set initial state with data from simplyRets
       setProperties(data)
       setLoading(false)
     }catch(error){
@@ -52,11 +57,16 @@ function PropertyList() {
       setLoading(false)
     }
   }
-
+  // fetch data only if it not exist in local storage
   useEffect(() => {
-    fetchProperties();
+    if (localStorage.getItem('properties')) {
+      return;
+    } else {
+      fetchProperties();
+    }
   }, [])
 
+  // handle when user select favorite property
   const handleFavorite = (id) => {
     localStorage.setItem('favorite', JSON.stringify([...favorite, id]));
     setFavorite([...favorite, id])
